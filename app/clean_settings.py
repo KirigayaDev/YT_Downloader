f554872ee81dc74cc7b72e2a3e8c2a5234165c5c
kirigaya <valerasaratov064@gmail.com>
2024-12-22 17:12:02 +0300

    улучшение валидации настроек

--- app/clean_settings.py
+++ app/clean_settings.py
@@ -2,6 +2,8 @@
 Парс и валидация конфигов
 """
 from pydantic import Field
+from pydantic.types import conint
+
 from pydantic_settings import BaseSettings
 
 
@@ -12,16 +14,16 @@ class BotSettings(BaseSettings):
     token: str = Field(alias='bot_token')
     api_id: str = Field()
     api_hash: str = Field()
-    parallel_download_count_limit: int = Field()
-    parallel_upload_count_limit: int = Field()
-    max_filesize: int = Field()
-    video_cache_ttl: int = Field()
-    parallel_upload_thumbnails_limit: int = Field()
+    parallel_download_count_limit: conint(ge=1) = Field()
+    parallel_upload_count_limit: conint(ge=1) = Field()
+    max_filesize: conint(ge=1, le=2147483648) = Field()
+    video_cache_ttl: conint(ge=0) = Field()
+    parallel_upload_thumbnails_limit: conint(ge=1) = Field()
 
 
 class RedisSettings(BaseSettings):
     host: str = Field(alias='redis_host')
-    port: int = Field(alias='redis_port')
+    port: conint(ge=0, le=65535) = Field(alias='redis_port')
 
 
 try:
